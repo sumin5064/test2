b9b9e1f5e427d867f6d4f080417acd992f52ff54
sumin5064 <sumin5064@gmail.com>
2026-06-01 01:17:24 +0900

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig, transformWithEsbuild } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [
+    {
+      name: "load-js-as-jsx",
+      async transform(code, id) {
+        if (!id.match(/src\/.*\.js$/)) return null;
+
+        return transformWithEsbuild(code, id, {
+          loader: "jsx",
+          jsx: "automatic"
+        });
+      }
+    },
+    react()
+  ]
+});
